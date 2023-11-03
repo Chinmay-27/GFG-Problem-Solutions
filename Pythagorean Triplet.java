b986d13cf6ea55b3bd4657de8a24ed5e9863b81b
Chinmay Raut <68195400+Chinmay-27@users.noreply.github.com>
2023-11-03 23:27:36 +0530

    Create Pythagorean Triplet.java

---
+++ Pythagorean Triplet.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    boolean checkTriplet(int[] arr, int n) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] * arr[i];
+            map.put(arr[i], i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (map.containsKey(arr[i] + arr[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
